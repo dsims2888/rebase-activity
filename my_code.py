fdbab41694b8f4dd39c37465da37a70c595fd760
dsims <dsims2888@aol.com>
2025-01-28 16:38:25 -0500

    commited!

--- my_code.py
+++ my_code.py
@@ -1,6 +1,10 @@
-def say_hello() -> None:
+def say_hello(n) -> None:
     print("hello world!");
-
+    for i in range(n-1):
+        print("hello world!");
 
 if __name__ == "__main__":
-    say_hello();
+    say_hello(3);
+
+
+
